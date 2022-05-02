c8fa967abeed9469a2d3ae3245cd8a29c0969b00
AnicetNgrt <an.nougaret@gmail.com>
2022-05-02 23:23:05 +0200

    renamed IPasswordHasher to PasswordHasher

--- src/Lib/ParswordHasher.h
+++ src/Lib/ParswordHasher.h
@@ -4,7 +4,7 @@
 
 using namespace std;
 
-class IPasswordHasher {
+class PasswordHasher {
 public:
   virtual string hash(string password) = 0;
   virtual bool verify(string password, string hash) = 0;
