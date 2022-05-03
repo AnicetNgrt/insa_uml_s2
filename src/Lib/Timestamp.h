a0a76a5f9ed6fe5c1b7ddd26f48bd7d9a741e833
Anicet Nougaret <anicet.nougaret@zohomail.eu>
2022-05-03 17:41:28 +0200

    fixed const Timestamp comparison issue

--- src/Lib/Timestamp.h
+++ src/Lib/Timestamp.h
@@ -14,4 +14,4 @@ struct Timestamp {
 };
 
 Timestamp timestamp_from_string(string s);
-bool timestamp_equal(Timestamp &t1, Timestamp &t2);
+bool timestamp_equal(Timestamp const &t1, Timestamp const &t2);
