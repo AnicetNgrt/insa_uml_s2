9cda185d79bd8bcb9663933254f8ccf0968e8b90
Anicet Nougaret <anicet.nougaret@zohomail.eu>
2022-05-03 13:48:14 +0200

    added service wip implementation

---
+++ src/Lib/Service.cpp
@@ -0,0 +1,38 @@
+#include "Service.h"
+
+Service::Service(Session &session) : session(session)
+{
+}
+
+Stream<Measurement>* Service::measurements()
+{
+}
+
+Result<double, const char*> Service::air_quality_area(double x, double y, double rad, Maybe<Timestamp> start, Maybe<Timestamp> end)
+{
+}
+
+Stream<Sensor>* Service::similar_sensors(string sensor_id, int n, Maybe<Timestamp> start, Maybe<Timestamp> end)
+{
+}
+
+Result<double, const char*> Service::cleaner_efficiency(string cleaner_id)
+{
+}
+
+Result<double, const char*> Service::provider_cleaners_efficiency(string provider)
+{
+}
+
+void Service::flag_owner(string owner_id, OwnerFlag flag)
+{
+}
+
+Maybe<const char*> Service::authenticate(string username, string password)
+{
+}
+
+Maybe<User> Service::authenticated_user()
+{
+}
+
---
+++ src/Lib/Service.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include "IService.h"
+#include "Session.h"
+
+class Service : public IService {
+public:
+  Service(Session &session);
+
+  Stream<Measurement> *measurements() override;
+  Result<double, const char *> air_quality_area(double x, double y, double rad,
+                                                Maybe<Timestamp> start,
+                                                Maybe<Timestamp> end) override;
+  Stream<Sensor> *similar_sensors(string sensor_id, int n,
+                                  Maybe<Timestamp> start,
+                                  Maybe<Timestamp> end) override;
+  Result<double, const char *> cleaner_efficiency(string cleaner_id) override;
+  Result<double, const char *>
+  provider_cleaners_efficiency(string provider) override;
+  void flag_owner(string owner_id, OwnerFlag flag) override;
+  Maybe<const char *> authenticate(string username, string password) override;
+  Maybe<User> authenticated_user() override;
+
+private:
+  Session &session;
+};
