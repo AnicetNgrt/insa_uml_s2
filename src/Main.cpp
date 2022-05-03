565acb40058fce4d85257eade6577a7d77ac6d87
Anicet Nougaret <anicet.nougaret@zohomail.eu>
2022-05-03 17:07:18 +0200

    fixed const issue

--- src/Main.cpp
+++ src/Main.cpp
@@ -5,9 +5,8 @@ using namespace std;
 #include "Lib/Interpreter.h"
 #include "Lib/Service.h"
 
-int main(int argc, char *argv[]) {
+int main(int argc, const char *argv[]) {
     Session session = from_args(argv, argc);
     Service service = Service(session);
     Interpreter interpreter = Interpreter(service);
-    
 }
