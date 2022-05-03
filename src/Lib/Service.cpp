0438dfa8b5c0898701a48d98a5f47ba89174adaa
MaelArr <arrive.mael@gmail.com>
2022-05-03 17:06:32 +0200

    flag_owner done

--- src/Lib/Service.cpp
+++ src/Lib/Service.cpp
@@ -6,14 +6,9 @@ Service::Service(Session &session) : session(session)
 
 Stream<Measurement>* Service::measurements(string sensor_id, Maybe<MeasurementType> type, Maybe<Timestamp> timestamp)
 {
-
 }
 
 Result<double, const char*> Service::air_quality_area(double x, double y, double rad, Maybe<Timestamp> start, Maybe<Timestamp> end)
-{
-}
-
-Result<double, const char*> Service::air_quality(double x, double y, Timestamp &at)
 {
 }
 
@@ -31,6 +26,13 @@ Result<double, const char*> Service::provider_cleaners_efficiency(string provide
 
 void Service::flag_owner(string owner_id, OwnerFlag flag)
 {
+	auto filter = [&](const Owner &owner) -> bool {
+		return owner.get_id() == owner_id;
+	};
+    auto owner_stream = session.owners_db.filter_and_stream(filter);
+    if(!owner_stream->receive().is_absent){
+    session.owners_flags.insert(make_pair(owner_id, flag));
+    }
 }
 
 Maybe<const char*> Service::authenticate(string username, string password)
