0ecbd27cca2168c6b2342e86153e78d4c210726e
Anicet Nougaret <anicet.nougaret@zohomail.eu>
2022-05-04 10:28:13 +0200

    added -d args to session parser

--- src/Lib/Session.cpp
+++ src/Lib/Session.cpp
@@ -8,9 +8,11 @@ Result<Session, string> from_args(const char **argv, size_t argc)
 	 * -h Affiche ce manuel (pas important ici)
 	 * -u Spécifie le nom d’utilisateur
 	 * -p Spécifie le mot de passe
+	 * -d Spécifie le répertoire du dataset (./dataset par défaut)
 	 ******************************/
 	string username = "";
 	string password = "";
+	string dataset_dir = "./dataset";
 
 	for (int i = 0; i < argc; ++i)
 	{
@@ -25,6 +27,11 @@ Result<Session, string> from_args(const char **argv, size_t argc)
 			if (i+1 > argc || argv[i+1][0] == '-') return Err("password is missing");
 			password = argv[++i];
 		}
+		else if (arg == "-d")
+		{
+			if (i+1 > argc || argv[i+1][0] == '-') return Err("dataset directory path is missing");
+			dataset_dir = argv[++i];
+		}
 	}
 	
 	if(username.length() == 0)
@@ -36,11 +43,11 @@ Result<Session, string> from_args(const char **argv, size_t argc)
 	Session opened_session = {
 		username,
 		password,
-		new DatabaseCSV<User>("dataset/users.csv"),
-		new DatabaseCSV<Cleaner>("dataset/cleaners.csv"),
-		new DatabaseCSV<Owner>("dataset/providers.csv"),
-		new DatabaseCSV<Measurement>("dataset/measurements.csv"),
-		new DatabaseCSV<Sensor>("dataset/sensors.csv"),
+		new DatabaseCSV<User>(dataset_dir + "/users.csv"),
+		new DatabaseCSV<Cleaner>(dataset_dir + "/cleaners.csv"),
+		new DatabaseCSV<Owner>(dataset_dir + "/providers.csv"),
+		new DatabaseCSV<Measurement>(dataset_dir + "/measurements.csv"),
+		new DatabaseCSV<Sensor>(dataset_dir + "/sensors.csv"),
 		None,
 		unordered_map<string, OwnerFlag>()
 	};
